13947a0e7219bf43615c59329b3bfb68dfa012a6
Marisol2201 <1529@holbertonschool.com>
2021-04-12 18:54:58 -0500

    array of dates added

--- src/main/java/IAS/HandyManCalculator/HandyManCalculatorApplication.java
+++ src/main/java/IAS/HandyManCalculator/HandyManCalculatorApplication.java
@@ -2,23 +2,28 @@ package IAS.HandyManCalculator;
 import IAS.HandyManCalculator.model.CalculateTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Vector;
 
 @SpringBootApplication
 public class HandyManCalculatorApplication {
 
 	public static void main(String[] args) {
-		Date initial = new Date();
 
-		System.out.println("This is from getTime in main + initial " + initial);
-		Date fin = new Date();
-		System.out.println("This is from getTime in main + fin" + fin);
+		Date date1 = new Date(1220227200);
+		Date date2 = new Date();
+
+		ArrayList dateList = new ArrayList();
+		dateList.add(date1);
+		dateList.add(date2);
+		ArrayList al = new ArrayList(dateList);
+		System.out.println(al);
 
 		//CalculateTime class
 		SpringApplication.run(HandyManCalculatorApplication.class, args);
-		CalculateTime time = new CalculateTime(initial, fin);
-		time.calculateWeekNumber(fin);
-		time.calculateHours(initial);
-		time.calculateHoursForEachDay(initial, fin);
+		CalculateTime time = new CalculateTime(al);
+		time.calculateWeekNumber(al);
 	}
 }
--- src/main/java/IAS/HandyManCalculator/model/CalculateTime.java
+++ src/main/java/IAS/HandyManCalculator/model/CalculateTime.java
@@ -1,46 +1,63 @@
 package IAS.HandyManCalculator.model;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class CalculateTime {
 
-    public CalculateTime(Date initialTime, Date finalTime) {
-        if(initialTime.before(finalTime) || (initialTime.equals(finalTime))) {
-            calculateWeekNumber(initialTime);
-            calculateWeekNumber(finalTime);
-            System.out.println("This is initialTime from the calculateTime class + initialTime: " + initialTime);
-            System.out.println("This is finalTime from the calculateTime class + finalTime: " + finalTime);
-        } else {
-            System.out.println("Error from CalculateTime class");
-        }
+    private final List<Date> date;
+
+    public CalculateTime(List<Date> date) {
+        this.date = date;
     }
 
-    public int calculateWeekNumber(Date date) {
+
+//        Calendar calendar = Calendar.getInstance();
+//        for (Date d: date
+//        ) {
+//            calendar.setTime(d);
+//            if(date.before(finalTime) || (initialTime.equals(finalTime))) {
+//                calculateWeekNumber(initialTime);
+//                System.out.println("This is initialTime from the calculateTime class + initialTime: " + initialTime);
+//                System.out.println("This is finalTime from the calculateTime class + finalTime: " + finalTime);
+//        }
+//        } else {
+//            System.out.println("Error from CalculateTime class");
+//        }
+//    }
+
+    public List<Date> calculateWeekNumber(List<Date> date) {
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        int weekNumber = calendar.get(Calendar.WEEK_OF_YEAR);
-        System.out.println("This is weekNumber from calculateWeek from the calculateTime class + weekNumber: " + weekNumber);
-        return weekNumber;
-    }
-
-    public int calculateHoursForEachDay(Date firstDate, Date secondDate) {
-        int sumHours = 0;
-        if(calculateWeekNumber(secondDate) != calculateWeekNumber(firstDate)) {
-            sumHours = (calculateHours(secondDate) - (calculateHours(firstDate)));
+        List<Date> result = new ArrayList<>();
+        for (Date d: date
+             ) {
+            calendar.setTime(d);
+            calendar.get(Calendar.WEEK_OF_YEAR);
         }
-        System.out.println("This is sumHours from calculateHoursForEachDay from the calculateTime class + sumHours: " + sumHours);
-        return sumHours;
+        //int weekNumber =
+        System.out.println("This is weekNumber from calculateWeek from the calculateTime class + weekNumber: " + result);
+        return result;
     }
 
-    public int calculateHours(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        int dayHour = calendar.get(Calendar.HOUR_OF_DAY);
-        System.out.println("This from calculateHours from the calculateTime class + dayHour: " + dayHour);
-        return dayHour;
-    }
-
-  //  public Date convertToMilitaryHour(Date date) {
+//    public int calculateHoursForEachDay(Date firstDate, Date secondDate) {
+//        int sumHours = 0;
+//        if(calculateWeekNumber(secondDate) != calculateWeekNumber(firstDate)) {
+//            sumHours = (calculateHours(secondDate) - (calculateHours(firstDate)));
+//        }
+//        System.out.println("This is sumHours from calculateHoursForEachDay from the calculateTime class + sumHours: " + sumHours);
+//        return sumHours;
+//    }
 //
-    //};
+//    public int calculateHours(Date date) {
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.setTime(date);
+//        int dayHour = calendar.get(Calendar.HOUR_OF_DAY);
+//        System.out.println("This from calculateHours from the calculateTime class + dayHour: " + dayHour);
+//        return dayHour;
+//    }
+//
+//  //  public Date convertToMilitaryHour(Date date) {
+////
+//    //};
 }
