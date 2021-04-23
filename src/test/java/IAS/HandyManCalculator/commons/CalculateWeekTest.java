667445f7879bb45c532047a850bbe8eadea2d995
Marisol2201 <1529@holbertonschool.com>
2021-04-23 10:06:15 -0500

    last commons folder changes

---
+++ src/test/java/IAS/HandyManCalculator/commons/CalculateWeekTest.java
@@ -0,0 +1,27 @@
+/*
+package IAS.HandyManCalculator.commons;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculateWeekTest {
+
+    @Test
+    void calculateWeekNumber() {
+        Date date1 = new Date(1618207200 * 1000L);
+        Date date2 = new Date(1618758000 * 1000L);
+        List list = new ArrayList();
+        list.add(date1);
+        list.add(date2);
+        assertEquals("No hay 2 elementos", 2, list.size());
+    }
+
+    @Test
+    void sumHours() {
+    }
+}*/
